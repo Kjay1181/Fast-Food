fdeadda454ca36adb2dc14a7ef6cc1a38ef9cb93
Kjay1181 <42454194+Kjay1181@users.noreply.github.com>
2018-09-04 12:31:43 -1000

    Add files via upload
    allows me to call upon an item in a property

---
+++ Hamburger.js
@@ -0,0 +1,15 @@
+var hamburger={
+meat:"beef",
+veg:["tomato","cucumber","pickles"],
+cheese:"american",
+mayo:true,
+mustard:true,
+ketchup:true,
+bbq:false,
+salt:true,
+pepper:false,
+price:7,
+qty:1
+
+}
+window.alert(hamburger.veg[1]);
